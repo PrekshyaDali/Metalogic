46055bfe46657d977df3353fb546b2100ff52ef3
Prekshya Dali <prekshyashrestha0@gmail.com>
2024-04-19 11:19:55 +0545

    toast

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
     <>
       <Router>
         <GlobalStyles></GlobalStyles>
-        <ToastContainer/>
+      
       </Router>
     </>
   );
--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -8,25 +8,10 @@ import { ToastContainer } from "react-toastify";
 export default function Layout({ children }) {
   const location = useLocation();
 
-  // Define metadata for each page
-  const pageMetadata = {
-    "/": {
-      title: "MetaLogic Home Page",
-      description: "Welcome to MetaLogic Software Pvt. Ltd.",
-      keywords:
-        "web development, responsive design, custom website, web application",
-      url: "https://metalogic.com.np/",
-      image: "https://metalogic.com.np/meta-image.jpg",
-    },
-  };
-
-  // Get metadata for the current page
-  const currentPageMetadata = pageMetadata[location.pathname];
-
   return (
     <>
       <div className="flex flex-col bg-[#D9D9D9] w-full">
-        <ToastContainer></ToastContainer>
+ 
         <NavBar />
         <main className="mt-0">
           <Outlet />
