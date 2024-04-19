e5a06148b32763e23a4645b2b6e1a77a3a5359b9
Prekshya Dali <prekshyashrestha0@gmail.com>
2024-04-19 13:26:48 +0545

    minor UI changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import MetaTag from "./MetaTag";
-import HoverMetaTag from "./HoverMetaTag";
+
 import { Link } from "react-router-dom";
 export default function Footer() {
   return (
